2272267edf46ed1f4b2d2485ed3a070bd2ea43a3
Sariah <sariahwandler@gmail.com>
2024-01-19 18:14:54 -0700

    design-principles.html

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,8 @@
 let dateModified = new Date(document.lastModified);
 let year = dateModified.getFullYear().toString();
 
-let text = document.querySelector("p").innerHTML;
+let text = document.querySelector("#copyright").innerHTML;
 copyright = text.replace("year", year);
-document.querySelector("p").innerHTML = copyright;
+document.querySelector("#copyright").innerHTML = copyright;
 
 document.querySelector("#lastModified").innerHTML = `Last Modifification: ${dateModified.toString()}`
