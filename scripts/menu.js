93afd69deac6facac8b005f8114b1ff9bfc9354f
Sariah <sariahwandler@gmail.com>
2024-01-19 22:58:45 -0700

    w3 enhance homepage

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('.navigation')
+const menubutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+menubutton.addEventListener('click', () => {
+	nav.classList.toggle('show');
+	menubutton.classList.toggle('show');
+});
