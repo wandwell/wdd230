699c787fa5341e0b4b2a83991ff98972f142f859
Sariah <sariahwandler@gmail.com>
2024-11-15 12:09:03 -0700

    add getdates js

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,4 +2,4 @@ const date = new Date();
 let year = date.getFullYear();
 
 document.querySelector('#year').innerHTML = year;
-document.querySelector(' footer p:last-child').textContent = `Last Modified: ${document.lastModified}`;
+document.querySelector('.copyright p:last-child').textContent = `Last Modified: ${document.lastModified}`;
